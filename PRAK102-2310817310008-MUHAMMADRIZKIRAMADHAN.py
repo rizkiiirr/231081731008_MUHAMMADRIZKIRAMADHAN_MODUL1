e4f97b72f438416941a7205628efaeb780ad8849
rizkiiirr <145253962+rizkiiirr@users.noreply.github.com>
2023-12-25 22:11:31 +0800

    Add files via upload

---
+++ PRAK102-2310817310008-MUHAMMADRIZKIRAMADHAN.py
@@ -0,0 +1,9 @@
+a = 4
+b = 8
+c = 3
+hasil = a * b / c
+
+print(f'Variabel a bernilai {a}')
+print(f'Variabel b bernilai {b}')
+print(f'Variabel c bernilai {c}')
+print(f'Hasil dari a dikali b dibagi c adalah {hasil:.6f}')   
